a884147735caea71ad32ebb6452aed3a832f2de2
simon <simon@dancingcloudservices.com>
2022-09-22 12:59:22 -0600

    Day 2 Lunch

--- src/main/java/studentsclean1/School.java
+++ src/main/java/studentsclean1/School.java
@@ -2,10 +2,15 @@ package studentsclean1;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 interface CriterionOfStudent {
   boolean test(Student s);
+}
+
+interface UseAStudent {
+  void accept(Student s);
 }
 
 public class School {
@@ -14,6 +19,12 @@ public class School {
       System.out.println("> " + s);
     }
     System.out.println("--------------------");
+  }
+
+  public static void useAllStudents(List<Student> roster, UseAStudent op) {
+    for (Student s : roster) {
+      op.accept(s);
+    }
   }
 
   public static List<Student> selectStudentsByCriterion(
@@ -39,5 +50,50 @@ public class School {
     showAllStudents(selectStudentsByCriterion(
         roster, s -> s.getCourses().size() < 3
     ));
+
+    // lab solutions:
+    // smart
+    showAllStudents(selectStudentsByCriterion(
+        roster, s -> s.getGpa() > 3.0));
+    // not smart
+    showAllStudents(selectStudentsByCriterion(
+        roster, s -> s.getGpa() <= 3.0));
+    // first half of alphabet
+    showAllStudents(selectStudentsByCriterion(
+        roster, s -> s.getName().toUpperCase().charAt(0) <= 'M'));
+
+    showAllStudents(selectStudentsByCriterion(
+        roster, s -> s.getName().compareTo("M") < 0));
+
+    // smart but not enthusiastic
+    showAllStudents(selectStudentsByCriterion(
+        roster, s -> s.getGpa() > 3.0 && s.getCourses().size() < 3
+    ));
+
+    // sorting
+    // Java 7 and older used Collections.sort(List<X>, Comparator<X>)
+    // since Java 8
+    // myList.sort(Comparator)
+    Comparator<Student> order =
+//        (Student s1, Student s2) -> Double.compare(s1.getGpa(), s2.getGpa());
+        (s1, s2) -> Double.compare(s1.getGpa(), s2.getGpa());
+//        (var s1, var s2) -> Double.compare(s1.getGpa(), s2.getGpa());
+    System.out.println("order by grade");
+    roster.sort(order);
+    System.out.println(roster);
+    System.out.println("order by reverse enthusiasm");
+    roster.sort((s1, s2) ->
+        Integer.compare(s2.getCourses().size(), s1.getCourses().size()));
+    System.out.println(roster);
+
+
+    System.out.println("use student simple print");
+    useAllStudents(roster, s -> System.out.println(s));
+
+    System.out.println("use pretty print");
+    useAllStudents(roster, s ->
+        System.out.println("Student: " + s.getName() + " takes "
+            + s.getCourses().size() + " courses and has a grade of "
+            + s.getGpa()));
   }
 }
